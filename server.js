8bdd5c7e789305404ce21408d8ece8b27a4bb32b
joeking9292 <noonanjoe92@gmail.com>
2020-05-29 23:12:03 -0700

    adding old server for old cs290 project

--- server.js
+++ server.js
@@ -1,35 +1,67 @@
+//Express server generating templates.
+
+
 var path = require('path');
 var express = require('express');
-var exphbs = require('express-handlebars');
 var app = express();
-var port = process.env.PORT || 3500;
+var exphbs = require('express-handlebars');
+var port = process.env.PORT || 3000;
+var categoryData = require('./categoryData');
+var recipeData = require('./recipeData');
+var fs = require('fs');
 
-var postData = require('./postData');
 
+//set handlebars to default for .render
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+//serve css and client.js
 app.use(express.static('public'));
 
-app.get(['/', '/index.html'], function(req, res, next) {
-    res.status(200).render('postPage', {
-        post: postData
+//  THIS SHOULD SERVE RECIPDATA.JSON TO INDEX.JS
+app.get('/recipes', function(req, res, next){
+
+  var recipeData = require('./recipeData.json');
+
+  res.json(recipeData);
+
+});
+
+
+
+
+// sends index with categoryData to the body of main.
+app.get('/index.html', function(req, res, next){
+  console.log(req.url, 'success')
+  res.render('index', {
+    categoryData: categoryData
+  })
+});
+
+app.get('/', function(req, res, next){
+  console.log(req.url, 'success')
+  res.render('index', {
+    categoryData: categoryData
+  })
+});
+
+app.get('/recipes.html', function(req, res, next){
+    console.log(req.url, 'success')
+    res.render('recipe', {
+        recipes: recipeData.recipes
     })
 });
 
-app.get('/posts/:number', function (req, res, next) {
-    var number = req.params.number;
-    if ((postData.length-1) < number) {
-        res.status(404).render('404', {});
-    } else {
-        res.status(200).render('partials/recipesCard', postData[number]);
+app.get("*", function (req, res) {
+    console.log(req.url, "not found");
+    res.status(404);
+    res.render('404');
+});
+
+
+app.listen(port, function (err) {
+    if (err) {
+      throw err;
     }
+    console.log("== Server listening on port", port);
   });
-
-app.get('*', function(req, res) {
-    res.status(404).render('404', {});
-});
-
-app.listen(port, function() {
-    console.log("== Server is listening on port", port);
-});
