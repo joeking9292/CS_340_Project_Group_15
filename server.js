19c4faa0f9ab7d50b66cd21118230db14b527deb
joeking9292 <noonanjoe92@gmail.com>
2020-05-29 22:56:19 -0700

    Setting up handlebars

---
+++ server.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+var port = process.env.PORT || 3500;
+
+var postData = require('./postData');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get(['/', '/index.html'], function(req, res, next) {
+    res.status(200).render('postPage', {
+        post: postData
+    })
+});
+
+app.get('/posts/:number', function (req, res, next) {
+    var number = req.params.number;
+    if ((postData.length-1) < number) {
+        res.status(404).render('404', {});
+    } else {
+        res.status(200).render('partials/recipesCard', postData[number]);
+    }
+  });
+
+app.get('*', function(req, res) {
+    res.status(404).render('404', {});
+});
+
+app.listen(port, function() {
+    console.log("== Server is listening on port", port);
+});
