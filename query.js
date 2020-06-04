0338a90398431797b2e595e4d2b6997a6b1ed556
joeking9292 <noonanjoe92@gmail.com>
2020-06-03 23:02:00 -0700

    Deleting unecessary files

--- query.js
+++
@@ -1,8 +0,0 @@
-var getConnection = require('./database');
-getConnection(function (err, con) {
-  if(err) { /* handle your error here */ }
-  var userQuery = 'select * from user';
-  console.log("con: " + con); //displays undefined
-  con.query(userQuery,function(err,user){
-  con.release();
-});
